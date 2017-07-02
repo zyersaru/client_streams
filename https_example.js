db31c021487cc2034e435e4f38286b7be39dbe17
Edmond <edmondcheung604@hotmail.com>
2017-07-02 01:08:31 +0000

    Added chunk to represent response data

--- https_example.js
+++ https_example.js
@@ -8,7 +8,11 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response:  ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
