ad9c5fe22940e9740078854d2c87ebbc4c9a38db
Edmond <edmondcheung604@hotmail.com>
2017-07-02 01:12:37 +0000

    Changed host and path to download Monty Python script

--- https_example.js
+++ https_example.js
@@ -1,8 +1,8 @@
 var https = require('https');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
