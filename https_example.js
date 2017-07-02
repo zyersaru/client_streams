ff1c6ba2e3a44e379b6e69c8523840c6edc98f3b
Edmond <edmondcheung604@hotmail.com>
2017-07-02 00:50:03 +0000

    Not expected?

--- https_example.js
+++ https_example.js
@@ -1,3 +1,17 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
